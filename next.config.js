c1f0be57d3572d0e650d9f89d858b32244d681b8
Hans Fredrik Furholt <hansff@gmail.com>
2019-06-04 20:12:34 +0200

    take 2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withTM = require('next-transpile-modules')
 
 module.exports = withTM({
   pageExtensions: ['jsx', 'js', 'bs.js'],
-  transpileModules: ['bs-platform'],
+  transpileModules: ['bs-platform', "reason-react"],
   target: "serverless"
 })
